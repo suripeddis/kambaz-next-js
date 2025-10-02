04c60e551cb420c29fe60a6ca427666a03be5df0
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-02 08:01:34 -0400

    fixed Kambaz link issue

--- app/(Kambaz)/page.tsx
+++ app/(Kambaz)/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
 export default function Kambaz() {
-    return (
-      <div id="wd-kambaz">
-        <h1>Kambaz</h1>
-      </div>
-  );}
-  
+  redirect("/Account/Signin");
+  return null;
+}

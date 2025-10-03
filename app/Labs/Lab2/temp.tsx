2dd297428fabba767221f4ae7b4e9225eec7c798
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-03 17:43:13 -0400

    Temp rename

---
+++ app/Labs/Lab2/temp.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Nav, NavItem, NavLink } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import CardImg from "react-bootstrap/CardImg";
+import CardBody from "react-bootstrap/CardBody";
+import CardTitle from "react-bootstrap/CardTitle";
+import CardText from "react-bootstrap/CardText";
+import Button from "react-bootstrap/Button";
+
+export default function BootstrapNavigation() {
+  return (
+    <>
+      <div id="wd-css-navigating-with-tabs">
+        <h2>Tabs</h2>
+        <Nav variant="tabs">
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Active">Active</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Link1">Link 1</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Link2">Link 2</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Disabled" disabled>
+              Disabled
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+
+      <div id="wd-css-navigating-with-cards">
+        <h2>Cards</h2>
+        <Card style={{ width: "18rem" }}>
+          <CardImg variant="top" src="images/stacked.jpg" />
+          <CardBody>
+            <CardTitle>Stacking Starship</CardTitle>
+            <CardText>
+              Stacking the most powerful rocket in history. Mars or bust!
+            </CardText>
+            <Button variant="primary">Boldly Go</Button>
+          </CardBody>
+        </Card>
+      </div>
+    </>
+  );
+}
--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,27 +1,41 @@
+"use client";
+
+import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
+
 export default function TOC() {
- return (
-   <ul>
-     <li>
-       <Link href="/Labs" id="wd-lab1-link">
-         Home </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab1" id="wd-lab1-link">
-         Lab 1 </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab2" id="wd-lab2-link">
-         Lab 2 </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab3" id="wd-lab3-link">
-         Lab 3 </Link>
-     </li>
-     <li>
-        <Link href="/Account/Signin" id="wd-kambaz-link">
+  return (
+    <Nav variant="pills">
+      <NavItem>
+        <NavLink href="/Labs" as={Link} id="wd-labs-link">
+          Home
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab1" as={Link} id="wd-lab1-link">
+          Lab 1
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab2" as={Link} id="wd-lab2-link">
+          Lab 2
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab3" as={Link} id="wd-lab3-link">
+          Lab 3
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/Account/Signin" as={Link} id="wd-kambaz-link">
           Kambaz
-        </Link>
-      </li>
-   </ul>
-);}
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/jannunzi" id="wd-github-link">
+          My GitHub
+        </NavLink>
+      </NavItem>
+    </Nav>
+  );
+}

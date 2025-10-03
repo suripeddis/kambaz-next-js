7908b2457be7998cf9a761765c7f6776f7e77dba
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-03 17:55:17 -0400

    Bootstrap complete need to fix image issue

--- app/Labs/Lab2/BootstrapNavigation.tsx
+++ app/Labs/Lab2/BootstrapNavigation.tsx
@@ -34,7 +34,7 @@ export default function BootstrapNavigation() {
       <div id="wd-css-navigating-with-cards">
         <h2>Cards</h2>
         <Card style={{ width: "18rem" }}>
-          <CardImg variant="top" src="images/stacked.jpg" />
+        <CardImg variant="top" src="/stacked.jpg" />
           <CardBody>
             <CardTitle>Stacking Starship</CardTitle>
             <CardText>

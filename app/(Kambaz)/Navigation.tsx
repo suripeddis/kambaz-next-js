62b9486a8a56a6c0af1336a1d50f33ddc00d4195
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-03 22:51:45 -0400

    Implement Kambaz navigation sidebar and layout fixes

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -2,72 +2,93 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default function CourseNavigation() {
   return (
-    <div
+    <ListGroup
       id="wd-courses-navigation"
-      className="list-group fs-5 rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+      className="wd fs-5 rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
       style={{ width: 200 }}
     >
-      <div className="text-center p-3">
-        <Image
-          src="/images/neu-logo.png"
-          width={120}
-          height={40}
-          alt="Northeastern University"
-        />
-      </div>
+      <ListGroupItem className="bg-black border-0 text-center">
+        <Link
+          href="https://www.northeastern.edu/"
+          target="_blank"
+          id="wd-neu-link"
+          className="d-inline-block"
+        >
+          <Image
+            src="/northeasternlogo.png"
+            width={120}
+            height={40}
+            alt="Northeastern University"
+          />
+        </Link>
+      </ListGroupItem>
 
-      <Link
+      <ListGroupItem
+        as={Link}
         href="/Courses/1234/Home"
         id="wd-course-home-link"
         className="list-group-item active border-0"
       >
         Home
-      </Link>
-      <Link
+      </ListGroupItem>
+
+      <ListGroupItem
+        as={Link}
         href="/Courses/1234/Modules"
         id="wd-course-modules-link"
         className="list-group-item text-danger border-0"
       >
         Modules
-      </Link>
-      <Link
+      </ListGroupItem>
+
+      <ListGroupItem
+        as={Link}
         href="/Courses/1234/Piazza"
         id="wd-course-piazza-link"
         className="list-group-item text-danger border-0"
       >
         Piazza
-      </Link>
-      <Link
+      </ListGroupItem>
+
+      <ListGroupItem
+        as={Link}
         href="/Courses/1234/Zoom"
         id="wd-course-zoom-link"
         className="list-group-item text-danger border-0"
       >
         Zoom
-      </Link>
-      <Link
+      </ListGroupItem>
+
+      <ListGroupItem
+        as={Link}
         href="/Courses/1234/Assignments"
         id="wd-course-assignments-link"
         className="list-group-item text-danger border-0"
       >
         Assignments
-      </Link>
-      <Link
+      </ListGroupItem>
+
+      <ListGroupItem
+        as={Link}
         href="/Courses/1234/Quizzes"
         id="wd-course-quizzes-link"
         className="list-group-item text-danger border-0"
       >
         Quizzes
-      </Link>
-      <Link
+      </ListGroupItem>
+
+      <ListGroupItem
+        as={Link}
         href="/Courses/1234/People/Table"
         id="wd-course-people-link"
         className="list-group-item text-danger border-0"
       >
         People
-      </Link>
-    </div>
+      </ListGroupItem>
+    </ListGroup>
   );
 }

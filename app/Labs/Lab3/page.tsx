220c8732f62f14c18f1b115ed5ab362b338e13bc
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-17 11:15:00 -0400

    3.3

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -5,6 +5,10 @@ import IfElse from "./IfElse";
 import TernaryOperator from "./TernaryOperator"
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInLine";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import SimpleArrays from "./SimpleArrays";
 
 export default function Lab3() {
   return(
@@ -17,6 +21,10 @@ export default function Lab3() {
       <TernaryOperator />
       <ConditionalOutputIfElse />
       <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <SimpleArrays />
     </div>
   );
 }

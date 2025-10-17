3c9be0331949f45eb56a8caae9b6e33c8a77ae2b
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-17 17:41:18 -0400

    fixing deployment issues

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import assignments from "../../../../Database/assignment.json";
+import assignmentsData from "../../../../Database/assignment.json";
 import Link from "next/link";
 import { Button, Form } from "react-bootstrap";
 
+type Assignment = {
+  _id: string;
+  title: string;
+  course: string;
+  description?: string;
+  points?: number;
+  dueDate?: string;       
+  availableDate?: string; 
+};
+
 export default function AssignmentEditor() {
-  const { cid, aid } = useParams();
-  const assignment = assignments.find(
-    (a: any) => a._id === aid && a.course === cid
-  );
+  const { cid, aid } = useParams<{ cid: string; aid: string }>();
+
+  const assignments = assignmentsData as Assignment[];
+  const assignment = assignments.find((a) => a._id === aid);
 
   if (!assignment) {
     return <p className="text-danger">Assignment not found.</p>;
@@ -17,28 +27,44 @@ export default function AssignmentEditor() {
 
   return (
     <div id="wd-assignment-editor" className="container mt-3">
-      <h3 className="text-danger">Edit Assignment</h3>
+      <h3 className="text-danger">{assignment.title}</h3>
       <Form>
-        {/* Assignment Title */}
         <Form.Group className="mb-3">
-          <Form.Label>Assignment Title</Form.Label>
+          <Form.Label>Title</Form.Label>
           <Form.Control type="text" defaultValue={assignment.title} />
         </Form.Group>
 
-        {/* Assignment ID */}
         <Form.Group className="mb-3">
-          <Form.Label>Assignment ID</Form.Label>
-          <Form.Control type="text" defaultValue={assignment._id} readOnly />
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={4}
+            defaultValue={assignment.description ?? ""}
+          />
         </Form.Group>
 
-        {/* Course ID */}
         <Form.Group className="mb-3">
-          <Form.Label>Course</Form.Label>
-          <Form.Control type="text" defaultValue={assignment.course} readOnly />
+          <Form.Label>Points</Form.Label>
+          <Form.Control
+            type="number"
+            defaultValue={assignment.points ?? 100}
+          />
         </Form.Group>
 
-        {/* Save / Cancel Buttons */}
-        <div className="d-flex justify-content-end gap-2 mt-4">
+        <Form.Group className="mb-3">
+          <Form.Label>Due Date</Form.Label>
+          <Form.Control type="date" defaultValue={assignment.dueDate ?? ""} />
+        </Form.Group>
+
+        <Form.Group className="mb-4">
+          <Form.Label>Available From</Form.Label>
+          <Form.Control
+            type="date"
+            defaultValue={assignment.availableDate ?? ""}
+          />
+        </Form.Group>
+
+        <div className="d-flex justify-content-end gap-2">
           <Link href={`/Courses/${cid}/Assignments`} className="btn btn-secondary">
             Cancel
           </Link>
--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,54 +1,62 @@
 "use client";
 
+import { useParams } from "next/navigation";
+import modulesData from "../../../Database/modules.json";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import { BsGripVertical } from "react-icons/bs";
 import ModulesControls from "./ModulesControls";
-import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
-import { useParams } from "next/navigation";
-import modules from "../../../Database/modules.json"; // ✅ fixed import
+import LessonControlButtons from "./LessonControlButtons";
+
+type Lesson = {
+  _id: string;
+  name: string;
+};
+
+type Module = {
+  _id: string;
+  name: string;
+  course: string;     // course id (cid)
+  lessons?: Lesson[];
+};
 
 export default function Modules() {
-  const { cid } = useParams();
+  const { cid } = useParams<{ cid: string }>();
+  const modules = modulesData as Module[];
+
+  const courseModules = modules.filter((m) => m.course === cid);
 
   return (
-    <div id="wd-modules-page">
+    <div>
       <ModulesControls />
-      <br />
-      <br />
-      <br />
-      <br />
+      <br /><br /><br /><br />
 
       <ListGroup id="wd-modules" className="rounded-0">
-        {modules
-          .filter((module: any) => module.course === cid)
-          .map((module: any) => (
-            <ListGroupItem
-              key={module._id}
-              className="wd-module p-0 mb-5 fs-5 border-gray"
-            >
-              <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" />
-                {module.name}
-                <ModuleControlButtons />
-              </div>
+        {courseModules.map((module) => (
+          <ListGroupItem
+            key={module._id}
+            className="wd-module p-0 mb-5 fs-5 border-gray"
+          >
+            <div className="wd-title p-3 ps-2 bg-secondary">
+              <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
+              <ModuleControlButtons />
+            </div>
 
-              {module.lessons && (
-                <ListGroup className="wd-lessons rounded-0">
-                  {module.lessons.map((lesson: any) => (
-                    <ListGroupItem
-                      key={lesson._id}
-                      className="wd-lesson p-3 ps-1"
-                    >
-                      <BsGripVertical className="me-2 fs-3" />
-                      {lesson.name}
-                      <LessonControlButtons />
-                    </ListGroupItem>
-                  ))}
-                </ListGroup>
-              )}
-            </ListGroupItem>
-          ))}
+            {module.lessons && module.lessons.length > 0 && (
+              <ListGroup className="wd-lessons rounded-0">
+                {module.lessons.map((lesson) => (
+                  <ListGroupItem
+                    key={lesson._id}
+                    className="wd-lesson p-3 ps-1"
+                  >
+                    <BsGripVertical className="me-2 fs-3" /> {lesson.name}{" "}
+                    <LessonControlButtons />
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            )}
+          </ListGroupItem>
+        ))}
       </ListGroup>
     </div>
   );

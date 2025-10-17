c12d1b13405c65d34764bdacd0730a49f8c3bb03
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-17 16:58:40 -0400

    didn't save the versions I fixed

--- app/Labs/Lab3/MapFunctions.tsx
+++ app/Labs/Lab3/MapFunctions.tsx
@@ -1,19 +1,20 @@
 export default function MapFunction() {
-  let numberArray1 = [1, 2, 3, 4, 5, 6];
+  const numberArray1 = [1, 2, 3, 4, 5, 6];
   const square = (a: number) => a * a;
   const todos = ["Buy milk", "Feed the pets"];
+
   const squares = numberArray1.map(square);
   const cubes = numberArray1.map((a) => a * a * a);
 
   return (
     <div id="wd-map-function">
       <h4>Map Function</h4>
-      squares = {squares.join(", ")} <br />
-      cubes = {cubes.join(", ")} <br />
+      squares = {squares} <br />
+      cubes = {cubes} <br />
       Todos:
       <ol>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+        {todos.map((todo, i) => (
+          <li key={i}>{todo}</li>
         ))}
       </ol>
       <hr />

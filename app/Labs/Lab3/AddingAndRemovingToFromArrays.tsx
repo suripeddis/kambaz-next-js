5b658d5f2cca8ca1f9043f8cbd79063c6829cfd2
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-17 17:11:09 -0400

    fixing vercel issues

--- app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
+++ app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
@@ -1,23 +1,24 @@
 export default function AddingAndRemovingToFromArrays() {
-    let numberArray1 = [1, 2, 3, 4, 5];
-    let stringArray1 = ["string1", "string2"];
-    let todoArray = [
-      <li key="buy-milk">Buy milk</li>,
-      <li key="feed-pets">Feed the pets</li>,
-    ];
+    const numberArray1 = [1, 2, 3, 4, 5];
+    const stringArray1 = ["string1", "string2"];
+    const todoArray = [<li key="1">Buy milk</li>, <li key="2">Feed the pets</li>];
   
     numberArray1.push(6);
     stringArray1.push("string3");
-    todoArray.push(<li key="walk-dogs">Walk the dogs</li>);
+    todoArray.push(<li key="3">Walk the dogs</li>);
+  
+    numberArray1.splice(2, 1);
+    stringArray1.splice(1, 1);
   
     return (
-      <div id="wd-adding-and-removing-to-from-arrays">
-        <h4>Adding and Removing to/from Arrays</h4>
-        numberArray1 = {numberArray1.join(", ")} <br />
-        stringArray1 = {stringArray1.join(", ")} <br />
+      <div id="wd-adding-removing-from-arrays">
+        <h4>Add/remove to/from arrays</h4>
+        numberArray1 = {numberArray1} <br />
+        stringArray1 = {stringArray1} <br />
         Todo list:
         <ol>{todoArray}</ol>
         <hr />
       </div>
     );
   }
+  
--- app/Labs/Lab3/MapFunctions.tsx
+++ app/Labs/Lab3/MapFunctions.tsx
@@ -2,7 +2,6 @@ export default function MapFunctions() {
   const numberArray1 = [1, 2, 3, 4, 5, 6];
   const square = (a: number) => a * a;
   const todos = ["Buy milk", "Feed the pets"];
-
   const squares = numberArray1.map(square);
   const cubes = numberArray1.map((a) => a * a * a);
 
@@ -13,9 +12,7 @@ export default function MapFunctions() {
       cubes = {cubes} <br />
       Todos:
       <ol>
-        {todos.map((todo, i) => (
-          <li key={i}>{todo}</li>
-        ))}
+        {todos.map((todo, i) => <li key={i}>{todo}</li>)}
       </ol>
       <hr />
     </div>

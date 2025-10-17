cbfd527075cea953c9a562212c5286fbc3620b89
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-17 17:44:46 -0400

    fixing deployment issues

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -2,29 +2,34 @@ import courses from "../../Database/courses.json";
 import { FaAlignJustify } from "react-icons/fa6";
 import CourseNavigation from "./Navigation";
 import Breadcrumb from "./Breadcrumb";
+import type { ReactNode } from "react";
 
-export default function CoursesLayout({
+type Course = { _id: string; name: string };
+
+export default async function CoursesLayout({
   children,
   params,
 }: {
-  children: React.ReactNode;
-  params: { cid: string };
+  children: ReactNode;
+  params: Promise<{ cid: string }>;
 }) {
-  const { cid } = params;
-  const course = courses.find((c) => c._id === cid);
+  const { cid } = await params; 
+  const course = (courses as Course[]).find((c) => c._id === cid);
 
   return (
     <div id="wd-courses">
-      <h2 className="text-danger d-flex align-items-center">
-        <FaAlignJustify className="me-3 fs-4 mb-1" />
-        <Breadcrumb course={course} />
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        {course ? course.name : "Course Not Found"}
       </h2>
-      <hr />
-      <div className="d-flex">
-        {/* Left Sidebar */}
-        <CourseNavigation cid={cid} />
+      <div className="text-secondary mb-3">
+        <Breadcrumb course={course} />
+      </div>
 
-        {/* Main Content Area */}
+      <hr />
+
+      <div className="d-flex">
+        <CourseNavigation cid={cid} />
         <div className="flex-grow-1 ms-5">{children}</div>
       </div>
     </div>

b920921f16a4f9892f34ee4b2689dc0d9edd1560
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-17 10:57:15 -0400

    3.2

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,6 +1,22 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import BooleanVariables from "./BooleanVariables";
+import VariableTypes from "./VariableTypes";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator"
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInLine";
+
 export default function Lab3() {
-  return (
-    <div>
-      <h2>Lab 3</h2>
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <BooleanVariables />
+      <VariableTypes />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
     </div>
-);}
+  );
+}

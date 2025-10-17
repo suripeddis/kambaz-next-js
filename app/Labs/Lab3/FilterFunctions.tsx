fd2ea304a6b9fe2e84b7269437991f616bcc76ae
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-10-17 16:53:42 -0400

    still fixing vercel issues

--- app/Labs/Lab3/FilterFunctions.tsx
+++ app/Labs/Lab3/FilterFunctions.tsx
@@ -1,6 +1,5 @@
 export default function FilterFunction() {
-    let numberArray1 = [1, 2, 4, 5, 6];
-  
+    const numberArray1 = [1, 2, 4, 5, 6];
     const numbersGreaterThan2 = numberArray1.filter((a) => a > 2);
     const evenNumbers = numberArray1.filter((a) => a % 2 === 0);
     const oddNumbers = numberArray1.filter((a) => a % 2 !== 0);
@@ -8,9 +7,9 @@ export default function FilterFunction() {
     return (
       <div id="wd-filter-function">
         <h4>Filter Function</h4>
-        numbersGreaterThan2 = {numbersGreaterThan2.join(", ")} <br />
-        evenNumbers = {evenNumbers.join(", ")} <br />
-        oddNumbers = {oddNumbers.join(", ")} <hr />
+        numbersGreaterThan2 = {numbersGreaterThan2}  <br />
+        evenNumbers = {evenNumbers}    <br />
+        oddNumbers = {oddNumbers}      <hr />
       </div>
     );
   }
--- app/Labs/Lab3/TernaryOperator.tsx
+++ app/Labs/Lab3/TernaryOperator.tsx
@@ -1,11 +1,9 @@
 export default function TernaryOperator() {
-    let loggedIn = true;
-  
+    const loggedIn = true;
     return (
       <div id="wd-ternary-operator">
         <h4>Logged In</h4>
-        {loggedIn ? <p>Welcome</p> : <p>Please login</p>}
-        <hr />
+        {loggedIn ? <p>Welcome</p> : <p>Please login</p>} <hr />
       </div>
     );
   }

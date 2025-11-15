03794156dd6fa5346e9ad80aa19106a6589a4c48
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-11-14 22:40:20 -0500

    fixing deployment issues

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 
 const axiosWithCredentials = axios.create({ withCredentials: true });
 export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER || "http://localhost:4000";
 export const USERS_API = `${HTTP_SERVER}/api/users`;
 
-export const signin = async (credentials) => {
+export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
   return response.data;
 };
 
-export const signup = async (user) => {
+export const signup = async (user: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
   return response.data;
 };
@@ -24,7 +25,7 @@ export const profile = async () => {
   return response.data;
 };
 
-export const updateUser = async (user) => {
+export const updateUser = async (user: any) => {
   const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
   return response.data;
 };
